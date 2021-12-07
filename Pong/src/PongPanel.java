0251ecd08b327e5f5757cac4344d8d9b4f11313b
TimParfenov <timonmih84@gmail.com>
2021-12-08 11:02:43 +1300

    Changes made:
    Font family = Dialog;
    Score font size = 100;
    Winner font size = 200;
    Ball = blue;

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -30,13 +30,13 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener{
 //	  Score numbers
 	  private final static int SCORE_TEXT_X = 100;
       private final static int SCORE_TEXT_Y = 100;
-      private final static int SCORE_FONT_SIZE = 50;
-      private final static String SCORE_FONT_FAMILY = "Serif";
+      private final static int SCORE_FONT_SIZE = 100;
+      private final static String SCORE_FONT_FAMILY = "Dialog";
       
       private final static int WINNER_TEXT_X = 200;
       private final static int WINNER_TEXT_Y = 200;
-      private final static int WINNER_FONT_SIZE = 40;
-      private final static String WINNER_FONT_FAMILY = "Serif";
+      private final static int WINNER_FONT_SIZE = 200;
+      private final static String WINNER_FONT_FAMILY = "Dialog";
       private final static String WINNER_TEXT = "WIN!";
 	
 	  
@@ -138,7 +138,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener{
 	      Graphics2D g2d = (Graphics2D) g.create();
 	         Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
 	         g2d.setStroke(dashed);
-	         g2d.setPaint(Color.WHITE);
+	         g2d.setPaint(Color.BLUE);
 	         g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
 	         g2d.dispose();
 	 }
